8c08626939c7ae129276bcb22c47bd1c91b8d81f
Janusz Jakubiak <Janusz.Jakubiak@pwr.edu.pl>
2025-03-18 10:50:28 +0100

    Simulation with scale parameter

--- src/bip_package/bip_package/imu_simulator.py
+++ src/bip_package/bip_package/imu_simulator.py
@@ -3,20 +3,25 @@ from rclpy.node import Node
 from geometry_msgs.msg import AccelStamped
 import time
 import random
+from rcl_interfaces.msg import ParameterDescriptor
 
 class Publisher(Node):
     def __init__(self):
         super().__init__('acc_publisher')
         self.publisher_= self.create_publisher(AccelStamped, 'acc_simulation', 10)
+        scale_descriptor = ParameterDescriptor(description='Scaling coefficient')
+        self.declare_parameter('scale', 1.0, scale_descriptor)
+
         self.timer = self.create_timer(1.0, self.timer_callback)
         self.msg_=AccelStamped()
         self.msg_.accel.linear.z=-1.0
         self.msg_.header.frame_id='map'
     
-    def timer_callback(self):        
-        dx=random.uniform(-0.05,0.05)
-        dy=random.uniform(-0.05,0.05)
-        dz=random.uniform(-0.1,0.1)
+    def timer_callback(self):
+        scale_param = self.get_parameter('scale').get_parameter_value().double_value
+        dx=random.uniform(-0.05*scale_param,0.05*scale_param)
+        dy=random.uniform(-0.05*scale_param,0.05*scale_param)
+        dz=random.uniform(-0.1*scale_param,0.1*scale_param)
         self.msg_.accel.linear.x+=dx
         self.msg_.accel.linear.y+=dy
         self.msg_.accel.linear.z+=dz
